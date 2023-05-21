6a3c2664cf55cc5aa737f12ba3f93b9d0289e6db
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-05-20 18:22:11 -0700

    complete lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -5,6 +5,7 @@ import React from "react";
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
+  
   render(<Article />);
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
